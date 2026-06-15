cf06db848a1b62e8d77b3a8c51d4b8b5fa15f478
Vincent <vtvo3@uci.edu>
2026-06-15 16:01:52 -0700

    added cast.py for casting the rod. edit later for better method of casting isntead of fixed time

---
+++ cast.py
@@ -0,0 +1,11 @@
+#this script is simply to cast the rod. the time to cast is fixed at 1 second.
+
+#import libraries
+import time
+import pyautogui
+
+def cast():
+    pyautogui.mouseDown()   # hold left click
+    time.sleep(1)           # wait for full power
+    pyautogui.mouseUp()   # release left click
+    time.sleep(0.5)     #half second delay before termination
