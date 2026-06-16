13348b2cdf0092ba34acc0e21e23a2638c56d033
Vincent <vtvo3@uci.edu>
2026-06-15 18:18:26 -0700

    added status messages

--- autofisher.py
+++ autofisher.py
@@ -6,9 +6,19 @@ import numpy
 
 import cast
 import fishing
+import minigame
 #delay til script actually runs
+print("script started, autofishing beginning in 5 seconds")
 time.sleep(5)
+print("script started")
 while True:
     cast.cast()
+    time.sleep(2)
     fishing.fish()
-    time.sleep(1)
+    #click
+    pyautogui.mouseDown()   # hold left click
+    time.sleep(0.5)           # wait for full power
+    pyautogui.mouseUp()
+    #time.sleep(1) 
+    #end of fish method means a fish has been detected
+    minigame.minigame()
