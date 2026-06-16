40cf10c6ee0fdba6def0f6bc6046425c40a18a4c
Vincent <vtvo3@uci.edu>
2026-06-15 21:26:14 -0700

    adjusted time to .93 seconds for perfect cast

--- cast.py
+++ cast.py
@@ -6,6 +6,6 @@ import pyautogui
 
 def cast():
     pyautogui.mouseDown()   # hold left click
-    time.sleep(0.87)        # wait for full power
+    time.sleep(0.93)        # wait for full power
     pyautogui.mouseUp()   # release left click
     time.sleep(2)     #half second delay before termination
